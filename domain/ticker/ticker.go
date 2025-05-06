5220c281519630913ff3082d7b75f07931be92c2
“jayjang0209” <jayjang0209@gmail.com>
2025-05-06 16:40:46 -0700

    feat: ticker pkg(version: accept clients and cache)

--- domain/ticker/ticker.go
+++ domain/ticker/ticker.go
@@ -1,13 +1,23 @@
 package ticker
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	sync "sync"
 	"time"
 
 	aedgrpc "github.com/sologenic/com-fs-aed-model"
+	aedclient "github.com/sologenic/com-fs-aed-model/client"
 	aedgrpcdmn "github.com/sologenic/com-fs-aed-model/domain"
+	assetgrpc "github.com/sologenic/com-fs-asset-model"
+	assetdmn "github.com/sologenic/com-fs-asset-model/domain"
 	assetdmnsymbol "github.com/sologenic/com-fs-asset-model/domain/symbol"
+	utilcache "github.com/sologenic/com-fs-utils-lib/go/cache"
+
+	"github.com/sologenic/com-fs-utils-lib/go/logger"
 	"github.com/sologenic/com-fs-utils-lib/models/metadata"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var (
@@ -19,6 +29,7 @@ var (
 const (
 	MaxTickerSymbolsNumber = 40
 	DefaultTickerPeriod    = 24 * time.Hour
+	DefaultCacheDuration   = 1 * time.Minute
 )
 
 type TickerReadOptions struct {
@@ -30,15 +41,35 @@ type TickerReadOptions struct {
 
 type Tickers map[string]*TickerPoint
 
+type Cache struct {
+	Mutex *sync.RWMutex
+	Data  map[string]*utilcache.LockableCache
+}
+
 func NewTickerReadOptions(symbols []string, to time.Time, period time.Duration) *TickerReadOptions {
 	return &TickerReadOptions{
-		Symbols: UniqueSymbols(symbols),
+		Symbols: uniqueSymbols(symbols),
 		To:      to,
 		Period:  period,
 	}
 }
 
-func UniqueSymbols(symbols []string) []string {
+func (opt *TickerReadOptions) Validate() error {
+	if len(opt.Symbols) == 0 {
+		return ErrTickerEmptySymbols
+	} else if len(opt.Symbols) > MaxTickerSymbolsNumber {
+		return ErrTickerTooManySymbols
+	}
+
+	for _, symb := range opt.Symbols {
+		if !validSymbol(symb) {
+			return ErrSymbolInvalid
+		}
+	}
+	return nil
+}
+
+func uniqueSymbols(symbols []string) []string {
 	keys := make(map[string]bool, len(symbols))
 	var res []string
 
@@ -51,29 +82,180 @@ func UniqueSymbols(symbols []string) []string {
 	return res
 }
 
-func ValidateSymbols(symbols []string) error {
-	if len(symbols) == 0 {
-		return ErrTickerEmptySymbols
-	} else if len(symbols) > MaxTickerSymbolsNumber {
-		return ErrTickerTooManySymbols
-	}
-
-	for _, symb := range symbols {
-		if !ValidSymbol(symb) {
-			return ErrSymbolInvalid
-		}
-	}
-	return nil
-}
-
-func ValidSymbol(symbol string) bool {
+func validSymbol(symbol string) bool {
 	_, err := assetdmnsymbol.NewSymbolFromString(symbol)
 	return err == nil
 }
 
-// Calculate the open, high, low, close, volume and invertedVolume
+func GetTickers(
+	ctx context.Context,
+	aedClient aedgrpc.AEDServiceClient,
+	assetClient assetgrpc.AssetListServiceClient,
+	opt *TickerReadOptions,
+	organizationID string,
+	c *Cache,
+) *TickerResponse {
+	retvals := getTickers(ctx, aedClient, assetClient, opt, organizationID, c)
+	tickers := tickersToHTTP(retvals, opt)
+	return tickers.ToResponse()
+}
+
+// Tickers to http evaluates the symbols to be either non inverted or inverted and switches the volume and invertedVolume accordingly
+func tickersToHTTP(tickers *Tickers, opt *TickerReadOptions) *Tickers {
+	retvals := make(Tickers)
+	for _, symbol := range opt.Symbols {
+		if _, ok := (*tickers)[symbol]; ok {
+			retvals[symbol] = (*tickers)[symbol]
+			continue
+		}
+	}
+
+	return &retvals
+}
+
+// Tickers are once calculated valid for up to refreshInterval seconds, however requests might come in in parallel and the cache might be empty, leading to multiple retrieval requests and subsequent caching of the same data.
+// We do however do not want to serialize the requests, since that could be slow or blocking.
+// The alternative used here, is that the actual cache is 15 seconds, and synchronized with the clock at refreshInterval to allow refreshes.
+// As long as there is demand for the data, somewhere in the 5 second interval beyond the wanted caching period, the data will be refreshed by placing a request a refresh channel.
+// This refresh channel is connect to blocking go routines per request type (symbol or group of symbols).
+//
+// Note: Since multiple instances of the service are running, the refresh is not 100% deduplicated: Another instances might attempt to refresh the cache at about the same time.
+func getTickers(
+	ctx context.Context,
+	aedClient aedgrpc.AEDServiceClient,
+	assetClient assetgrpc.AssetListServiceClient,
+	opt *TickerReadOptions,
+	organizationID string,
+	c *Cache,
+) *Tickers {
+	// Retrieve the tickers from the AED service:
+	tickerPoints := make(map[string]*TickerPoint)
+
+	AEDs := make([]*aedgrpc.AEDs, 0, len(opt.Symbols))
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+	for _, symbol := range opt.Symbols {
+		// Cache check for the symbol:
+		c.Mutex.RLock()
+		if cache, ok := c.Data[symbol]; ok {
+			v := cache.Value.(*TickerPoint)
+			tickerPoints[symbol] = v
+			c.Mutex.RUnlock()
+			continue
+		}
+		c.Mutex.RUnlock()
+		wg.Add(1)
+		go func(symbol string) {
+			baseAEDs, err := getAED(ctx, aedClient, assetClient, symbol, opt, organizationID)
+			if err != nil {
+				logger.Errorf("(no cache) Error getting aed data for %s: %s", symbol, err.Error())
+				wg.Done()
+				return
+			}
+			mutex.Lock()
+			AEDs = append(AEDs, baseAEDs)
+			mutex.Unlock()
+			wg.Done()
+		}(symbol)
+	}
+	wg.Wait()
+	tickersResp := AEDsToTickers(AEDs, opt, tickerPoints, c)
+	return (*Tickers)(&tickersResp)
+}
+
+// Retrieve the AED data from the source
+func getAED(
+	ctx context.Context,
+	aedClient aedgrpc.AEDServiceClient,
+	assetClient assetgrpc.AssetListServiceClient,
+	symbol string,
+	opt *TickerReadOptions,
+	organizationID string,
+) (*aedgrpc.AEDs, error) {
+	loadSymbol := &aedgrpc.AEDFilter{
+		Symbol:         symbol,
+		Network:        opt.Network,
+		Period:         &aedgrpc.Period{Type: aedgrpc.PeriodType_PERIOD_TYPE_HOUR, Duration: 1},
+		To:             timestamppb.New(opt.To),
+		From:           timestamppb.New(opt.To.Add(-opt.Period)),
+		Backfill:       true,
+		AllowCache:     true,
+		OrganizationID: organizationID,
+	}
+	baseAEDs, err := aedClient.Get(aedclient.AuthCtx(ctx), loadSymbol)
+	if err != nil {
+		return nil, fmt.Errorf("error getting aed data for %s: %w", symbol, err)
+	}
+	// Prevent downstream failures on empty arrays.
+	if len(baseAEDs.AEDs) == 0 {
+		return nil, fmt.Errorf("no aed data found for %s", symbol)
+	}
+	// Normalize the AED data
+	for _, aed := range baseAEDs.AEDs {
+		var err error
+		aed, err = normalizeAED(ctx, assetClient, aed, organizationID)
+		if err != nil {
+			logger.Errorf("Error normalizing AED %v: %v", aed, err)
+			continue
+		}
+	}
+	return baseAEDs, nil
+}
+
+func normalizeAED(
+	ctx context.Context,
+	assetClient assetgrpc.AssetListServiceClient,
+	aed *aedgrpc.AED,
+	organizationID string,
+) (*aedgrpc.AED, error) {
+	symbol, err := assetdmnsymbol.NewSymbolFromString(aed.Symbol)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse symbol: %w", err)
+	}
+	// Get base asset precision
+	baseAsset, err := assetClient.GetAsset(ctx, &assetgrpc.AssetKey{
+		Key: assetdmn.CreateAssetKeyStr(symbol.Base, organizationID),
+	})
+	if err != nil || baseAsset.AssetDetails == nil || baseAsset.AssetDetails.Denom == nil {
+		return nil, fmt.Errorf("failed to fetch base precision for %v: %w", symbol.Base, err)
+	}
+	// Get quote asset precision
+	quoteAsset, err := assetClient.GetAsset(ctx, &assetgrpc.AssetKey{
+		Key: assetdmn.CreateAssetKeyStr(symbol.Quote, organizationID),
+	})
+	if err != nil || quoteAsset.AssetDetails == nil || quoteAsset.AssetDetails.Denom == nil {
+		return nil, fmt.Errorf("failed to fetch quote precision for %v: %w", symbol.Quote, err)
+	}
+
+	return aedgrpcdmn.NormalizeAED(
+		ctx,
+		aed,
+		organizationID,
+		baseAsset.AssetDetails.Denom.Precision,
+		quoteAsset.AssetDetails.Denom.Precision,
+	)
+}
+
+// AEDsToTickers converts the aed data to ticker data
+// The values calculated are cached for refreshInterval max (clock rounded to refreshInterval intervals) with an allowed stale period of 5s, giving an always retrieval of values
+// from the cache for performance and data cost reasons (we can refresh in a go blocking routine while the data is still being served quickly)
+func AEDsToTickers(AEDs []*aedgrpc.AEDs, domainOptions *TickerReadOptions, tickerPoints map[string]*TickerPoint, c *Cache) map[string]*TickerPoint {
+	for _, aed := range AEDs {
+		tickerPoint := calculateTickerAED(aed, domainOptions)
+		tickerPoints[aed.AEDs[0].Symbol] = tickerPoint
+		c.Mutex.Lock()
+		c.Data[aed.AEDs[0].Symbol] = &utilcache.LockableCache{
+			Value:       tickerPoint,
+			LastUpdated: time.Now(),
+		}
+		c.Mutex.Unlock()
+	}
+	return tickerPoints
+}
+
+// calculate the open, high, low, close, volume and invertedVolume
 // Returns a single AED with the calculated values.
-func NewTickerPointFromAEDs(AEDs *aedgrpc.AEDs, options *TickerReadOptions) *TickerPoint {
+func calculateTickerAED(AEDs *aedgrpc.AEDs, options *TickerReadOptions) *TickerPoint {
 	if len(AEDs.AEDs) == 0 {
 		return nil
 	}
@@ -81,7 +263,6 @@ func NewTickerPointFromAEDs(AEDs *aedgrpc.AEDs, options *TickerReadOptions) *Tic
 	// get the base AEDs for the requested period calculation.
 	// The input data contains the base data to calculate the requested period over.
 	// Calculate the volume:
-	// Calculate the aggregate values from the AEDs
 	var open, close, low, high, volume, invertedVolume, firstPrice, marketCap, eps, per, yield float64
 	// Assumption is that the data might not be ordered by time.
 	var tStart, tEnd time.Time
@@ -164,14 +345,4 @@ func (t *Tickers) ToResponse() *TickerResponse {
 		resp.Tickers = append(resp.Tickers, ticker)
 	}
 	return resp
-}
-
-func (t *Tickers) FilterBySymbols(symbols []string) *Tickers {
-	result := make(Tickers)
-	for _, symbol := range symbols {
-		if ticker, ok := (*t)[symbol]; ok {
-			result[symbol] = ticker
-		}
-	}
-	return &result
 }
